0775400430e105ce5d65f784cf6d7e88b422be89
thepattj <jdej261@gmail.com>
2018-05-17 13:36:19 -0500

    Mod
    Se arreglaron los detalles de los laterales, se agrego el carrusel.
    
    Falta el mediaquery

--- index.php
+++ index.php
@@ -24,17 +24,55 @@
 				<div class="textM col-md-4"><a href="#here"> Ubicación </a> </div>
 			</div>
 		</header>
-		<div class="logo col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-			<img src="images/inicio1.jpg">
-			<img src="images/inicio2.jpg" style="display: none;">
-			<img src="images/inicio3.jpg" style="display: none;">
-			<img src="images/inicio4.jpg" style="display: none;">
-
-			<a href="">&#10094</a>
-			<a href="">&#10095</a>
+		
+		<!--EL CARRUSEL DE IMAGENES-->
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>
+		<div class="logo col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
+			<div id="my-slider" class="carousel slide" data-ride="carousel">
+			    <!--indicators dot nov-->
+			    <!--wrapper for slides-->
+			    <div class="carousel-inner" role="listbox">
+			        <div class="item active">
+			            <img src="images/inicio0.jpg" alt="inicio1" id="logoimg">
+			            <div class="carousel-caption">
+			                <h1>Un lugar con gran prestigio</h1>
+			            </div>
+			        </div>
+			        <div class="item">
+			            <img src="images/inicio2.jpg" alt="inicio2" id="logoimg">
+			            <div class="carousel-caption">
+			                <h1>Nuestro servicio, te hara sentir en casa</h1>
+			            </div>
+			        </div>
+			        <div class="item">
+			            <img src="images/inicio3.jpg" alt="inicio3" id="logoimg">
+			            <div class="carousel-caption">
+			                <h1>Amor por la comida</h1>
+			            </div>
+			        </div>
+			         <div class="item">
+			            <img src="images/inicio4.jpg" alt="inicio4" id="logoimg">
+			            <div class="carousel-caption">
+			                <h1>Visitanos, te enamoraras de nosotros</h1>
+			            </div>
+			        </div>
+			    </div>
+			    <!--controls or next and prev buttons-->
+			    <a class="left carousel-control" href="#my-slider" role="button" data-slide="prev">
+			        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			        <span class="sr-only">Previous</span>
+			    </a>
+			    <a class="right carousel-control" href="#my-slider" role="button" data-slide="next">
+			        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			        <span class="sr-only">Next</span>
+			    </a>
+			</div>
 		</div>
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>
+		<!--LA HISTORIA-->
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><h1 id="us"></h1></div>
-		<div class="historia col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>		
+		<div class="historia col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
 			<h1>Historia</h1>
 			<div class="hst col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
@@ -43,18 +81,26 @@
 				<img src="images/History2.jpg" >
 			</div>
 		</div>
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>
 
-
-		<div class="historia col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-			<h1 id="here">Ubicacion</h1>
+        <!--EL MAPA-->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xl-offset-12"><h1 id="here"></h1></div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xl-offset-12"><h1 id="here"></h1></div>
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>		
+		<div class="historia col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
+			<h1>Ubicación</h1>
 			<div class="hst col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam sint, ad deserunt id cumque, fuga iusto voluptatum quam modi placeat, vero odit quae sequi, explicabo amet. Saepe perspiciatis facere tempora.</p>
-				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus reiciendis, eveniet officiis delectus atque quisquam consequuntur, illum tempora nulla aliquid error ad, magni magnam sit nostrum harum iste. Exercitationem, error. </p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
 			</div>
-			<div class="hst col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6" id="map">
-				
+			<div class="hsti col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+				<div class="hst col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" id="map"> </div>
 			</div>
 		</div>
+		<div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>
+		
+		
+		<!--EL FOOTER-->
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xl-offset-12"><h1 id="here"></h1></div>
 		<footer class="footer col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 			<ul>
 				<a href="https://www.facebook.com/briochemx/"> <li><img src="images/icon/facebook.png">Facebook</li> </a>				
